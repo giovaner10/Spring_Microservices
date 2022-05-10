f723599b4b95fac84d1462397982016eb7a92fe6
giovaner10 <giovanerr10@gmail.com>
2022-05-10 11:23:18 -0300

    model de pagamento

---
+++ hr-payroll/src/main/java/com/microservices/hrpayroll/entities/Payment.java
@@ -0,0 +1,22 @@
+package com.microservices.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+
+    public double getTotal() {
+        return days * dailyIncome;
+    }
+}
