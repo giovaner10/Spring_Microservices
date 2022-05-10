17e9b418a2d1d841bdfe571891acea52f994dc4f
giovaner10 <giovanerr10@gmail.com>
2022-05-10 09:45:56 -0300

    config

---
+++ rh-work/src/main/java/com/microservices/rhwork/RhWorkApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.rhwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhWorkApplication.class, args);
+    }
+
+}
